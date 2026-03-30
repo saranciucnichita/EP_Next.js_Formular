7b6c82fe411d81d909c43c8b3cc346747861f9a3
saranciucnichita <saranciucnichita@gmail.com>
2026-03-30 11:09:53 +0300

    Fix path warning, add input fields

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+      // Sets the root to the directory containing this config file
+      root: __dirname,
+    },
 };
 
 export default nextConfig;
---
+++ src/features/home/index.jsx
@@ -0,0 +1,27 @@
+"use client";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function HomeComponent() {
+    return (
+        // h-screen -> height: 100vh;
+        <div className="flex items-center justify-center h-screen bg-gray-200">
+
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+            >
+                <h1>Evenimente</h1>
+                <TextField label="ID" variant="outlined" />
+                <TextField label="Titlu" variant="outlined" />
+                <TextField label="Locație" variant="outlined" />
+                <TextField label="Descriere" variant="outlined" />
+                <Button variant="contained">Upload</Button>
+
+            </Box>
+        </div>
+    );
+}
